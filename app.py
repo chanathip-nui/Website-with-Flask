77b75f5ba2b1824eb4fb6ac1a6499fba32eb8995
chanathip_nuisee <chanathip.nuis@gmail.com>
2026-02-28 18:14:25 +0700

    Add icon app

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ def index():
             "name": "Photo Editor",
             "category": "Photography",
             "rating": "4.8",
-            "icon_url": "https://via.placeholder.com/150",
+            "icon_url": "https://img.icons8.com/fluency/48/photo-editor.png",
         },
         {
             "name": "Fitness Tracker",
             "category": "Health",
             "rating": "4.5",
-            "icon_url": "https://via.placeholder.com/150",
+            "icon_url": "https://img.icons8.com/3d-fluency/94/apple-watch.png",
         },
         # Add more...
     ]
-    return render_template("index.html", apps=apps_data)
+    return render_template("main/index.html", apps=apps_data)
 
 
 if __name__ == "__main__":
